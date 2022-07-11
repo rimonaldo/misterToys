437e07b5aa49d127f2434d93a5ce82a295d18bce
Rimon <rimons2804@gmail.com>
2022-07-11 14:34:40 +0300

    first commit

---
+++ src/components/header.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <section class="header">
+        <h1>header</h1>
+    </section>
+
+</template>
+ <script>
+export default {
+    name: 'ProjectApp',
+    components: {},
+    data() {
+        return {};
+    },
+    created() { },
+    methods: {},
+    computed: {},
+    unmounted() { },
+};
+</script>
+ <style>
+ /* .header {
+    line-height: 4rem;
+    background-color: gray;
+ } */
+ </style>
